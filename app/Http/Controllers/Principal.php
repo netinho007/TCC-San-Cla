0c8f53ff8517a9479c4d33f5299778a47576f15b
netinho007 <miguelantoniodesouzaneto00@gmail.com>
2025-09-19 17:13:39 -0300

    Primeiro Commit

---
+++ app/Http/Controllers/Principal.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Principal extends Controller
+{
+    function principal(){
+        echo 'Página Principal';
+    }
+}
+
+

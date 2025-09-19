6fc9593ff431c16090042e06d4d6199c6fff9184
netinho007 <miguelantoniodesouzaneto00@gmail.com>
2025-09-19 17:44:49 -0300

    add paginas

---
+++ app/Http/Controllers/CadastroController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\cadastro;
+use App\Http\Requests\StorecadastroRequest;
+use App\Http\Requests\UpdatecadastroRequest;
+
+class CadastroController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorecadastroRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(cadastro $cadastro)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(cadastro $cadastro)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatecadastroRequest $request, cadastro $cadastro)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(cadastro $cadastro)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/ServicosController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\servicos;
+use App\Http\Requests\StoreservicosRequest;
+use App\Http\Requests\UpdateservicosRequest;
+
+class ServicosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreservicosRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(servicos $servicos)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(servicos $servicos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateservicosRequest $request, servicos $servicos)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(servicos $servicos)
+    {
+        //
+    }
+}
